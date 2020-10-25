cb65cd45ac2365e2dc50024ca9e0d51fc67105c2
DartPower <SIMPSON-NEKEK@mail.ru>
2020-10-25 04:01:06 +0300

    Blyat

--- __LibAstral/__LibAstral/Program.cs
+++ __LibAstral/__LibAstral/Program.cs
@@ -4,46 +4,37 @@ using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace __LibAstral
 {
     class Program
     {
         [STAThread]
-        [HandleProcessCorruptedStateExceptions]
-        [SecurityCritical]
         static void Main(string[] args)
         {
             Marshal.PrelinkAll(typeof(Program));
-
-            IntPtr hProv = (IntPtr)0;
-            
-            [System.Runtime.InteropServices.DllImport("advapi32.dll")]
-            static extern bool CryptGenRandom(IntPtr hProv, int dwLen, byte[] pbBuffer);
-
-            byte[] newRandom = new byte[10];
-
-            [DllImport("kernel32")]
-            static extern int HeapSize(int hHeap, int flags);
-
-            Guid guid = Guid.NewGuid();
-            ulong blyat = 2147483647;
-            var cyka = guid.ToString().Split((char)blyat);
-
-            var random = new Random();
-            int number = random.Next(Int32.MinValue, Int32.MaxValue);
-            uint uintNumber = (uint)(number + (uint)Int32.MaxValue);
-
-            if (!CryptGenRandom(hProv, (int)(uint)(newRandom.Length), newRandom))
+            while (true)
             {
-                while (true)
-                {
-                    Console.ForegroundColor = GetRandomConsoleColor();
-                    //Console.Write(Marshal.ReadInt32((IntPtr)HeapSize(Convert.ToInt32(cyka),0)));
-
-                    Console.Write(Marshal.ReadInt32((IntPtr)HeapSize(number, 0)));
-                }
+                Console.BackgroundColor = GetRandomConsoleColor();
+                Console.ForegroundColor = GetRandomConsoleColor();
+                Console.Write(GenRandomString(Guid.NewGuid().ToString("n").Substring(0, 8), 1));
             }
+        }
+
+        private static string GenRandomString(string Alphabet, int Length)
+        {
+            Random rnd = new Random();
+            StringBuilder sb = new StringBuilder(Length - 1);
+            int Position = 0;
+
+            for (int i = 0; i < Length; i++)
+            {
+                Position = rnd.Next(0, Alphabet.Length - 1);
+                sb.Append(Alphabet[Position]);
+            }
+
+            return sb.ToString();
         }
 
         private static Random _random = new Random();
